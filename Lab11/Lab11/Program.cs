a449ec21d6e3f747caa00cd8db57f4898f8dbcc6
nagalight <57606060+nagalight@users.noreply.github.com>
2021-01-20 16:52:23 +0700

    Add files via upload

---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int a = 1; a <= n - i; a++)
+                    Console.Write(" ");
+                Console.Write("*");
+                for (int a = 1; a < i; a++)
+                    Console.Write(" *");
+                Console.WriteLine();
+            }
+            for (int i = n-1; i > 0; i--)
+            {
+                for (int a = 1; a <= n - i; a++)
+                    Console.Write(" ");
+                Console.Write("*");
+                for (int a = 1; a < i; a++)
+                    Console.Write(" *");
+                Console.WriteLine();
+            }
+        }
+    }
+}
